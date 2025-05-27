84eec7194b65250b665044e721eeb2f5cd17f9b3
Zak <djabazaki@gmail.com>
2025-05-28 00:07:41 +0100

    working clock

--- JS/clock.js
+++ JS/clock.js
@@ -1,7 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-
+  // first
   const clock = document.getElementById("clock");
-  const now = new Date();
-  
-  clock.textContent =  now.toLocaleTimeString() + " GMT+1"; // returns in HH:MM:SS format
-});
+
+  function getTime() {
+    const now = new Date();
+    clock.textContent = now.toLocaleTimeString() + " GMT+1"; // returns in HH:MM:SS format
+  }
+
+  getTime();
+
+  //then
+  const intervalID = setInterval(getTime, 1000);
